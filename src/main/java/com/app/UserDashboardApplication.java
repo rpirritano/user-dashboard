adff5e39d980318f020db32350b4be49fcc446f6
Robert Pirritano <rpirritano@gmail.com>
2020-06-02 14:08:48 -0700

    first commit

---
+++ src/main/java/com/app/UserDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDashboardApplication.class, args);
+	}
+
+}
